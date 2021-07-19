022aa8e46f4f2b4a2c9035f4039e629a6721000c
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-07-19 19:59:54 -0300

    feat: add footer and container to layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,21 @@
 import { ThemeProvider } from 'styled-components';
 
+import Footer from 'components/footer';
+// import Header from 'components/header';
+
 import { GlobalStyle } from '../styles/global';
 import { theme } from '../styles/theme';
+import * as S from './styles';
 import { ILayout } from './interfaces';
 
 export default function Layout({ children }: ILayout) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <main>{children}</main>
+      <S.Layout>
+        <S.Container>{children}</S.Container>
+        <Footer />
+      </S.Layout>
     </ThemeProvider>
   );
 }
