1dc405b8062cfe844a1f2eefe34149255a4426b8
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-07-19 19:59:25 -0300

    feat: add initial footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './stylers';
+
+export default function Footer() {
+  return (
+    <S.Footer>
+      <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
+        Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
+      </a>
+    </S.Footer>
+  );
+}
---
+++ src/components/footer/stylers.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+  align-items: center;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  height: 100px;
+  justify-content: center;
+  width: 100%;
+
+  > a {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+
+    > img {
+      margin-left: 0.5rem;
+    }
+  }
+`;
