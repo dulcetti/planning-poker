e7e79a58eab2c02cfc7c58f2ff585d7d75d4f02d
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-07-19 20:00:32 -0300

    style(layout): add initial styles

---
+++ src/components/layout/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 0 0.5rem;
+`;
+
+export const Container = styled.main`
+  align-items: center;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 5rem 0;
+`;
