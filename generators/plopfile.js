b055f6abd9bc398ebaf40bda72a872e5ef6c0943
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-07-25 20:52:54 -0300

    chore: add plopfile generator

---
+++ generators/plopfile.js
@@ -0,0 +1,34 @@
+module.exports = (plop) => {
+  plop.setGenerator('components', {
+    description: 'Create a Component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{dashCase name}}/{{dashCase name}}.tsx',
+        templateFile: 'templates/component.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{dashCase name}}/styles.ts',
+        templateFile: 'templates/styles.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{dashCase name}}/interface.ts',
+        templateFile: 'templates/interface.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{dashCase name}}/spec.ts',
+        templateFile: 'templates/spec.hbs',
+      },
+    ],
+  });
+};
